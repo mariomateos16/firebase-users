cae7d32d58d9375abe11b4c495ffa52ff558e73d
Mario Mateos <mariomateosmateos@gmail.com>
2024-04-27 18:13:56 +0200

    updated

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+        "projectId":"users-angular-e9f24",
+        "appId":"1:686354682682:web:868a9974bbbfe1144df603",
+        "storageBucket":"users-angular-e9f24.appspot.com",
+        "apiKey":"AIzaSyAUPxebH3N6uotgwqgy1gnJsWNGZ4nBno0",
+        "authDomain":"users-angular-e9f24.firebaseapp.com",
+        "messagingSenderId":"686354682682"
+    },
+};
